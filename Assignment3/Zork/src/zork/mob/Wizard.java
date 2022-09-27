36f6e266b1a75c02c51f0fab83c6f0cc4cafb96f
Worawit Penglam <82023896+PoonwannadoCoding@users.noreply.github.com>
2022-09-28 01:14:25 +0700

    Add files via upload

---
+++ Assignment3/Zork/src/zork/mob/Wizard.java
@@ -0,0 +1,36 @@
+package io.muic.gigadot.ssc.zork.mob;
+
+import java.io.Serializable;
+
+public class Wizard extends Monster {
+    public Wizard() {
+        super("Wizard",
+                "Welcome to the Underground Kingdom. \n" + "The place that you will DIE!!!!",
+                200,
+                200,
+                40,
+                0.8,
+                0.1,
+                "              _,-'|\n" +
+                        "           ,-'._  |\n" +
+                        " .||,      |####\\ |\n" +
+                        "\\.`',/     \\####| |\n" +
+                        "= ,. =      |###| |\n" +
+                        "/ || \\    ,-'\\#/,'`.\n" +
+                        "  ||     ,'   `,,. `.\n" +
+                        "  ,|____,' , ,;' \\| |\n" +
+                        " (3|\\    _/|/'   _| |\n" +
+                        "  ||/,-''  | >-'' _,\\\\\n" +
+                        "  ||'      ==\\ ,-'  ,'\n" +
+                        "  ||       |  V \\ ,|\n" +
+                        "  ||       |    |` |\n" +
+                        "  ||       |    |   \\\n" +
+                        "  ||       |    \\    \\\n" +
+                        "  ||       |     |    \\\n" +
+                        "  ||       |      \\_,-'\n" +
+                        "  ||       |___,,--\")_\\\n" +
+                        "  ||         |_|   ccc/\n" +
+                        "  ||        ccc/\n" +
+                        "  ||              ");
+    }
+}
